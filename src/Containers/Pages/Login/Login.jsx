802fa3ee75dde236b3213989fd118c30ca07c192
amyzhou2402 <61258541+amyzhou2402@users.noreply.github.com>
2022-01-12 12:22:26 +1100

    new login

--- src/Containers/Pages/Login/Login.jsx
+++ src/Containers/Pages/Login/Login.jsx
@@ -1,4 +1,32 @@
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Login.module.scss";
 const Login = () => {
-    return <h1>login</h1>;
+    return (
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label className="label">Name</Form.Label>
+                <Form.Control type="name" placeholder="Enter name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Remember Me  " />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    );
 };
 export default Login;
