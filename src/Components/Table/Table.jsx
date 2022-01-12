62ccc61d058401fe385df8b5e9e9cd415bdb00cb
freddydude02 <frederickkeke@gmail.com>
2022-01-12 17:58:46 +1100

    mailing-list-btn

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,83 +1,86 @@
 //import data from "../../data";
 import "./Table.scss";
-import Table from 'react-bootstrap/Table';
-import ModalFunc from '../Modal';
-import axios from 'axios';
-import {useState, useEffect} from 'react';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-
-
+import { Button, Table } from "react-bootstrap";
+import ModalFunc from "../Modal";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const TableData = () => {
-    
-  const [show, setShow] = useState(false);
-  const [data,setData] = useState(null);
+    const [show, setShow] = useState(false);
+    const [data, setData] = useState(null);
 
-  const callFunction = () => {
-    try{
-      axios
-        .get(
-            "https://4026a4upzi.execute-api.ap-southeast-2.amazonaws.com/hello"
-        )
-        .then((result) =>{
-          let latestUpdates = [];
-          for(let i=0; i< 10;i++){
-            latestUpdates.push(result.data[i]);
-          }
-          setData(latestUpdates);
-          console.log(latestUpdates);
-        })
-    }
-    catch(e){
-      console.log(e);
-    }
-    
-};
+    const callFunction = () => {
+        try {
+            axios
+                .get(
+                    "https://4026a4upzi.execute-api.ap-southeast-2.amazonaws.com/hello"
+                )
+                .then((result) => {
+                    let latestUpdates = [];
+                    for (let i = 0; i < 10; i++) {
+                        latestUpdates.push(result.data[i]);
+                    }
+                    setData(latestUpdates);
+                    console.log(latestUpdates);
+                });
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
-useEffect(callFunction, []);
+    useEffect(callFunction, []);
 
-    const handleClose = () =>{
-      setShow(false)
+    const handleClose = () => {
+        setShow(false);
     };
     const handleShow = () => {
-      setShow(true);
-     
-    }
+        setShow(true);
+    };
 
-  return (
-      <div className="tableDiv">
-      {
-        data ? <Table striped bordered hover>
-        <thead>
-        <tr>
-                <th>Library Name</th>
-                <th>Version Number</th>
-                <th>Link</th>
-                <th>Notification</th>
-            </tr>
-        </thead>
-        <tbody>
-        {
-          data.map((update) => (
-                <tr key = {update.versionNumber}>
-                    <td key = {update.libraryName}>{update.libraryName}</td>
-                    <td key = {update.versionNumber}>{update.versionNumber}</td>
-                    <td key = {update.link}><a className="linkClass" target="_blank" rel="noreferrer" href="https://docs.splunk.com/Documentation/Splunk/latest/ReleaseNotes/MeetSplunk">{update.link}</a></td>
-                    <td><NotificationsIcon onClick={handleShow} /></td> 
-                </tr>
+    return (
+        <div className="tableDiv">
+            {data ? (
+                <>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Library Name</th>
+                                <th>Version Number</th>
+                                <th>Link</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((update) => (
+                                <tr key={update.versionNumber}>
+                                    <td key={update.libraryName}>
+                                        {update.libraryName}
+                                    </td>
+                                    <td key={update.versionNumber}>
+                                        {update.versionNumber}
+                                    </td>
+                                    <td key={update.link}>
+                                        <a
+                                            className="linkClass"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                            href="https://docs.splunk.com/Documentation/Splunk/latest/ReleaseNotes/MeetSplunk"
+                                        >
+                                            {update.link}
+                                        </a>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                    <Button onClick={handleShow}>Add me to Mail list</Button>
+                </>
+            ) : (
+                <h1>Loading...</h1>
+            )}
 
-            ))} 
-        </tbody>
-            
-        </Table> : <h1>Loading..</h1>
-      }
-        
-        <ModalFunc show={show} handleClose={handleClose} />
-      </div>
-        
+            <ModalFunc show={show} handleClose={handleClose} />
+        </div>
     );
 };
-
-
 
 export default TableData;
